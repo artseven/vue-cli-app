e6e07a4577e7f10fb14b20a8004608fc4bbef725
artseven <yabkevich.artsiom.09@gmail.com>
2019-11-21 12:20:19 -0500

    adding filter, removing some of the scaffold

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+Vue.filter("capitalize", string => {
+  let [first, ...tail] = string;
+  return first.toUpperCase() + tail.join("");
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
